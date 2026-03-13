64f8822f80c23b2d3c198e48b3a0e24625d51bcc
vitorchibere <vitorsantos14@icloud.com>
2026-03-12 22:05:03 -0300

    controller criado

---
+++ trabalho/src/main/java/com/vitor/trabalho/controller/ProdutoController.java
@@ -0,0 +1,40 @@
+package com.vitor.trabalho.controller;
+
+
+
+import com.vitor.trabalho.model.Produto;
+import com.vitor.trabalho.service.ProdutoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoService service;
+
+    public ProdutoController(ProdutoService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public Produto cadastrar(@RequestBody Produto produto) {
+        return service.salvar(produto);
+    }
+
+    @GetMapping
+    public List<Produto> listar() {
+        return service.listar();
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id, @RequestBody Produto produto) {
+        return service.atualizar(id, produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable Long id) {
+        service.deletar(id);
+    }
+}
